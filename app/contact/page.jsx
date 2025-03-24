0b16558ec74dd826b54113b515a8a4f93ae0d3e7
raymondkings <raymond.setia@tum.de>
2025-03-24 19:44:54 +0700

    text improvement

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -49,11 +49,8 @@ const Contact = () => {
         <div className="container mx-auto">
           <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
             <div className="flex flex-col justify-center">
-              <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
-                <span className="w-[30px] h-[2px] bg-primary"></span>
-                Say Hello 👋
-              </div>
               <h1 className="h1 max-w-md mb-8">Contact me</h1>
+              <caption className="caption max-w-md mb-8">For hiring purposes, please fill out this form to connect with me.</caption>
             </div>
 
             <div>
